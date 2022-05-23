3937a4f1540785770e1d814921f8f61067a57c3b
TijnBerns <tijn_berns@hotmail.com>
2022-05-23 17:49:33 +0200

    combining psoCNN with NTK

--- algorithm/score_networks.py
+++ algorithm/score_networks.py
@@ -2,6 +2,9 @@
 # import dataset
 
 # run get_ntk_n()
+import sys
+sys.path.append('../algorithm')
+
 import torch
 from torch import nn
 from torch.utils.data import TensorDataset
@@ -9,10 +12,11 @@ from torchvision.datasets import MNIST
 from torchvision.transforms import transforms
 import torch.nn.functional as F
 
-from algorithm.fitness.lib.linear_regions import get_linear_regions
-from algorithm.fitness.lib.ntk import get_ntk_n, get_ntk_score
-from algorithm.particle import Particle
+from linear_regions import get_linear_regions
+from ntk import NTK 
+from particle import Particle
 
+device = "cpu"
 
 class NetMnist(nn.Module):
     def __init__(self):
@@ -46,37 +50,35 @@ def get_networks(n):
     for i in range(n):
         particle = Particle(min_layer=3, max_layer=20, max_pool_layers=m_pool, input_width=28, input_height=28,
                             input_channels=1, conv_prob=0.6, pool_prob=0.3, fc_prob=0.1, max_conv_kernel=7,
-                            max_out_ch=256, max_fc_neurons=300, output_dim=10, device='cuda')
+                            max_out_ch=256, max_fc_neurons=300, output_dim=10, device=device)
         particle.model_compile(dropout_rate=0.5)
         model = particle.model
-        model.cuda()
         nets.append(model)
 
     return nets
 
 
 def get_dataloader(batch_size=16):
-    root = './data'
+    root = '../data'
     trans = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (1.0,))])
-    train_set = MNIST(root=root, train=True, transform=trans, download=True)
+    train_set = MNIST(root=root, train=True, transform=trans, download=False)
     #train_set.train_data.to(torch.device("cuda"))
     #train_set.train_labels.to(torch.device("cuda"))
     #tensor_set = TensorDataset(train_set)
 
     dataloader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=False, pin_memory=True, drop_last=True)
-
     return dataloader
-
 
 #networks = get_networks(2)
 network = NetMnist()
+network.to(device)
 networks = get_networks(1)
-network.cuda()
+# network.cuda()
 print(network)
 dataloader = get_dataloader()
 
 #ntk = get_ntk_n(dataloader, networks, num_batch=1)
-ntk = get_ntk_score(dataloader, networks[0], 1)
+ntk = NTK(device).get_ntk_score(dataloader, networks[0], 1)
 print(ntk)
 #linear_regions = get_linear_regions(dataloader, networks)
 #print(linear_regions)
